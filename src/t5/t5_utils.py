70faf74ebc91338cc4694d2411f04f4f9c423ab4
ghimiresunil <sunilghimire64@gmail.com>
2023-09-15 14:47:59 +0545

    Add: Utils for T5

---
+++ src/t5/t5_utils.py
@@ -0,0 +1,196 @@
+import os
+import torch
+import pickle
+import logging
+import pandas as pd
+from os import truncate
+from typing import Tuple
+from tqdm.auto import tqdm
+from multiprocessing import Pool
+from datasets import load_dataset
+from torch.utils.data import Dataset
+from datasets import Dataset as HFDataset
+from transformers import PreTrainedTokenizer
+from tokenizers.processors import BertProcessing
+from tokenizers.implementations import ByteLevelBPETokenizer
+
+logger = logging.getLogger(__name__)
+
+def preprocess_batch_for_hf_dataset(dataset, tokenizer, args):
+    if args.preprocess_inputs:
+        model_inputs = tokenizer(
+            text=[
+                prefix + ": " + input_text
+                for prefix, input_text in zip(dataset["prefix"], dataset["input_text"])
+            ],
+            max_length=args.max_seq_length,
+            padding="max_length",
+            return_tensors="np",
+            truncation=True,
+        )
+        with tokenizer.as_target_tokenizer():
+            labels = tokenizer(
+                text=dataset["target_text"],
+                max_length=args.max_seq_length,
+                padding="max_length",
+                return_tensors="np",
+                truncation=True,
+            )
+        model_inputs["labels"] = labels["input_ids"]
+
+        return model_inputs
+    else:
+        model_inputs = tokenizer(
+            text=[
+                prefix + input_text
+                for prefix, input_text in zip(dataset["prefix"], dataset["input_text"])
+            ],
+            max_length=args.max_seq_length,
+            padding="max_length",
+            return_tensors="np",
+            truncation=True,
+        )
+        with tokenizer.as_target_tokenizer():
+            labels = tokenizer(
+                text=dataset["target_text"],
+                max_length=args.max_seq_length,
+                padding="max_length",
+                return_tensors="np",
+                truncation=True,
+            )
+        model_inputs["labels"] = labels["input_ids"]
+
+        return model_inputs
+
+
+def load_hf_dataset(data, tokenizer, args):
+    if isinstance(data, str):
+        dataset = load_dataset(
+            "csv",
+            data_files=data,
+            delimiter="\t",
+            download_mode="force_redownload"
+            if args.reprocess_input_data
+            else "reuse_dataset_if_exists",
+        )
+    else:
+        dataset = HFDataset.from_pandas(data)
+
+    dataset = dataset.map(
+        lambda x: preprocess_batch_for_hf_dataset(x, tokenizer=tokenizer, args=args),
+        batched=True,
+    )
+
+    dataset.set_format(type="pt", columns=["input_ids", "attention_mask"])
+
+    if isinstance(data, str):
+        # This is not necessarily a train dataset. The datasets library insists on calling it train.
+        return dataset["train"]
+    else:
+        return dataset
+
+
+def preprocess_data(data):
+    prefix, input_text, target_text, tokenizer, args = data
+
+    # Add EOS again if truncated?
+    if args.preprocess_inputs:
+        batch = tokenizer(
+            text=[prefix + ": " + input_text],
+            max_length=args.max_seq_length,
+            padding="max_length",
+            return_tensors="np",
+            truncation=True,
+        )
+        with tokenizer.as_target_tokenizer():
+            labels = tokenizer(
+                text=[target_text],
+                max_length=args.max_seq_length,
+                padding="max_length",
+                return_tensors="np",
+                truncation=True,
+            )
+        batch["labels"] = labels["input_ids"]
+    else:
+        batch = tokenizer(
+            text=[prefix + input_text],
+            max_length=args.max_seq_length,
+            padding="max_length",
+            return_tensors="np",
+            truncation=True,
+        )
+        with tokenizer.as_target_tokenizer():
+            labels = tokenizer(
+                text=[target_text],
+                max_length=args.max_seq_length,
+                padding="max_length",
+                return_tensors="np",
+                truncation=True,
+            )
+        batch["labels"] = labels["input_ids"]
+    input_ids = batch["input_ids"][0]
+    attention_mask = batch["attention_mask"][0]
+    labels = batch["labels"][0]
+    return (input_ids, attention_mask, labels)
+
+
+class T5Dataset(Dataset):
+    def __init__(self, tokenizer, args, data, mode):
+        cached_features_file = os.path.join(
+            args.cache_dir,
+            args.model_name.replace("/", "_")
+            + "_cached_"
+            + str(args.max_seq_length)
+            + str(len(data)),
+        )
+
+        if os.path.exists(cached_features_file) and (
+            (not args.reprocess_input_data and not args.no_cache)
+            or (mode == "dev" and args.use_cached_eval_features and not args.no_cache)
+        ):
+            logger.info(" Loading features from cached file %s", cached_features_file)
+            with open(cached_features_file, "rb") as handle:
+                self.examples = pickle.load(handle)
+        else:
+            logger.info(" Creating features from dataset file at %s", args.cache_dir)
+
+            data = [
+                (prefix, input_text, target_text, tokenizer, args)
+                for prefix, input_text, target_text in zip(
+                    data["prefix"], data["input_text"], data["target_text"]
+                )
+            ]
+
+            if (mode == "train" and args.use_multiprocessing) or (
+                mode == "dev" and args.use_multiprocessing_for_evaluation
+            ):
+                if args.multiprocessing_chunksize == -1:
+                    chunksize = max(len(data) // (args.process_count * 2), 500)
+                else:
+                    chunksize = args.multiprocessing_chunksize
+
+                with Pool(args.process_count) as p:
+                    self.examples = list(
+                        tqdm(
+                            p.imap(preprocess_data, data, chunksize=chunksize),
+                            total=len(data),
+                            disable=args.silent,
+                        )
+                    )
+            else:
+                self.examples = [
+                    preprocess_data(d) for d in tqdm(data, disable=args.silent)
+                ]
+
+            if not args.no_cache:
+                logger.info(
+                    " Saving features into cached file %s", cached_features_file
+                )
+                with open(cached_features_file, "wb") as handle:
+                    pickle.dump(self.examples, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, index):
+        return self.examples[index]
