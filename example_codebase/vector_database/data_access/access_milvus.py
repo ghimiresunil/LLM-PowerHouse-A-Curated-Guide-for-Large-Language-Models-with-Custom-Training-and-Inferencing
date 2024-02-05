f5fc4bd28fd7cada8ba1908098f910dc39c4a722
ghimiresunil <sunilghimire64@gmail.com>
2024-02-05 12:54:35 +0545

    Update: CRUD Operation

--- example_codebase/vector_database/data_access/access_milvus.py
+++ example_codebase/vector_database/data_access/access_milvus.py
@@ -1,21 +1,20 @@
 from pymilvus import connections
 
+
 class MilvusDBConnection(object):
-    def __init__(self, alias = 'default'):
+    def __init__(self, alias="default"):
         host = "localhost"
         port = 19530
         self.alias = alias
         self.host = host
         self.port = port
         self.connetion = None
-        
+
     def _build(self):
         self.connetion = connections.connect(
-            alias = self.alias,
-            host= self.host,
-            port = self.port
+            alias=self.alias, host=self.host, port=self.port
         )
-    
+
     def get_connections(self):
         if self.connetion is None:
             RuntimeError("Start the database connection!")
@@ -23,7 +22,7 @@ class MilvusDBConnection(object):
 
     def start(self):
         self._build()
-    
+
     def stop(self):
         if self.connetion is not None:
             self.connetion.disconnect()
--- example_codebase/vector_database/data_access/utils.py
+++ example_codebase/vector_database/data_access/utils.py
@@ -3,6 +3,7 @@ from pymilvus import utility
 from pymilvus import Collection, DataType, CollectionSchema, FieldSchema
 from data_access.collection_base import MilvusCollectionBase
 
+
 class ClassificationCollection(MilvusCollectionBase):
     def __init__(self):
         self.collection_name = "TicketClassification"
@@ -22,44 +23,37 @@ class ClassificationCollection(MilvusCollectionBase):
         index_params = {
             "index_type": "IVF_FLAT",
             "metric_type": "IP",
-            "params": {
-                "nlist": 1024
-            }
+            "params": {"nlist": 1024},
         }
         id_field = FieldSchema(
-            name="id",
-            dtype=DataType.INT64,
-            is_primary=True,
-            auto_id=True
+            name="id", dtype=DataType.INT64, is_primary=True, auto_id=True
         )
 
         product_purchased_field = FieldSchema(
-            name="product_purchased",
-            dtype=DataType.VARCHAR,
-            max_length=100
+            name="product_purchased", dtype=DataType.VARCHAR, max_length=100
         )
 
         ticket_subject_field = FieldSchema(
-            name="ticket_subject",
-            dtype=DataType.VARCHAR,
-            max_length=100
+            name="ticket_subject", dtype=DataType.VARCHAR, max_length=100
         )
-        
+
         ticket_description_field = FieldSchema(
-            name="ticket_description",
-            dtype=DataType.VARCHAR,
-            max_length=1000
+            name="ticket_description", dtype=DataType.VARCHAR, max_length=1000
         )
-        
+
         ticket_description_embeddings_field = FieldSchema(
-            name="ticket_description_embeddings",
-            dtype=DataType.FLOAT_VECTOR,
-            dim=768
+            name="ticket_description_embeddings", dtype=DataType.FLOAT_VECTOR, dim=768
         )
 
         schema = CollectionSchema(
-            fields=[id_field, product_purchased_field, ticket_subject_field, ticket_description_field, ticket_description_embeddings_field],
-            description="Ticket Collection For Testing."
+            fields=[
+                id_field,
+                product_purchased_field,
+                ticket_subject_field,
+                ticket_description_field,
+                ticket_description_embeddings_field,
+            ],
+            description="Ticket Collection For Testing.",
         )
 
         collection = Collection(
@@ -67,12 +61,11 @@ class ClassificationCollection(MilvusCollectionBase):
             schema=schema,
             using="default",
             shards_num=2,
-            consistency_level="Strong"
+            consistency_level="Strong",
         )
 
         collection.create_index(
-            field_name="ticket_description_embeddings",
-            index_params=index_params
+            field_name="ticket_description_embeddings", index_params=index_params
         )
 
         self.collection = collection
@@ -80,62 +73,69 @@ class ClassificationCollection(MilvusCollectionBase):
 
     def insert(self, data):
         if isinstance(data, pd.DataFrame):
-            data = data.to_dict(orient='records')
-        
+            data = data.to_dict(orient="records")
+
         try:
             insert_response = self.collection.insert(data)
             return True
         except Exception as e:
             print("Error while inserting data")
             return False
-        
+
     def delete(self, expr):
-        count = self.collection.delete(
-            expr
-        )
+        count = self.collection.delete(expr)
         print("Total number of deleted items: ", count)
         return True
-    
-    def hybrid_search(self, embeddings, anns_field:str, content_name:str=None, top_k: int = 5):
-        search_params = {
-            "metric_type":"IP",
-            "params":{
-                "nprobe":16
-            }
-        }
 
-        if content_name:
+    def hybrid_search(
+        self,
+        embeddings,
+        anns_field: str,
+        product_purchased: str = None,
+        ticket_subject: str = None,
+        top_k: int = 5,
+    ):
+        search_params = {"metric_type": "IP", "params": {"nprobe": 16}}
+
+        if product_purchased and ticket_subject:
             print("content_name found")
             search_result = self.collection.search(
-                data = embeddings,
+                data=embeddings,
                 anns_field=anns_field,
-                param = search_params,
-                limit = top_k,
-                expr = f'content_name == \"{content_name}\"',
-                # expr = None,
-                output_fields = ["id","content_name", "content"]
+                param=search_params,
+                limit=top_k,
+                expr=f'product_purchased == "{product_purchased}" AND ticket_subject == "{ticket_subject}"',
+                output_fields=[
+                    "id",
+                    "product_purchased",
+                    "ticket_subject",
+                    "ticket_description",
+                ],
             )
         else:
-            print("content_name not found")
+            print("product_purchased and ticket_subject not found")
             search_result = self.collection.search(
-                data = embeddings,
+                data=embeddings,
                 anns_field=anns_field,
-                param = search_params,
-                limit = top_k,
-                output_fields = ["id", "content_name", "content"]
-            )   
+                param=search_params,
+                limit=top_k,
+                output_fields=[
+                    "id",
+                    "product_purchased",
+                    "ticket_subject",
+                    "ticket_description",
+                ],
+            )
         return search_result
 
     def get_primary_keys_associated(self, product_purchased, ticket_subject):
         exp = f'product_purchased == "{product_purchased}" and ticket_subject == "{ticket_subject}"'
         results = self.collection.query(expr=exp)
-        primary_key_list = [result['id'] for result in results]
+        primary_key_list = [result["id"] for result in results]
         return primary_key_list
-    
+
     def is_content_exist(self, product_purchased, ticket_subject):
         exp = f'product_purchased == "{product_purchased}" and ticket_subject == "{ticket_subject}"'
         res = self.collection.query(expr=exp)
         print("Count:", len(res))
         return len(res) > 0
-
-    
